4f2063002cbc94b10d2826a657b3f7bf8aef500e
Matthias Bernard <mb95@arcor.de>
2022-11-05 17:06:30 +0100

    Show cursor in stream and some refactoring

--- src/screenshare/__init__.py
+++ src/screenshare/__init__.py
@@ -1,4 +1,4 @@
-from pynput import keyboard
+from pynput import keyboard, mouse
 from mss import mss
 import cv2
 import numpy as np
@@ -7,6 +7,8 @@ import socket
 
 
 from ._version import __version__
+
+
 
 DEFAUL_PORT = 8000
 
@@ -17,6 +19,7 @@ def main():
     parser.add_argument('--host', type=str, help='IP or hostname which acts as display server' )
     parser.add_argument('--port', type=int, default=DEFAUL_PORT, help='Port of the display server')
     parser.add_argument('--compression', choices=['jpg', 'png'], default='jpg', help='Image compression used for streaming')
+    parser.add_argument('--hide_cursor', action='store_true')
 
     parser.add_argument('--scale', type=float, default=None, help='Resolution scaling')
 
@@ -114,81 +117,142 @@ def main():
 
         class KeyboardState:
             def __init__(self):
-                self.selected_monitor_idx = -1
-                self.pressed_keys = set()
-        
+                self._selected_monitor_idx = -1
+                self._pressed_keys = set()
+
+                digit_keys = [keyboard.KeyCode(char=str(i)) for i in range(10)]
+                f_keys = [keyboard.Key.f9, keyboard.Key.f10, keyboard.Key.f11, keyboard.Key.f12]
+                self._hotkeys = {key:idx for idx, key in sum([list(enumerate(keys)) for keys in [digit_keys, f_keys]], [])} 
+
+            def on_press(self, key):
+                self._pressed_keys.add(key)
+                if keyboard.Key.alt_l in self._pressed_keys and key in self._hotkeys:
+                    selected_monitor_idx = self._hotkeys[key]
+                    if selected_monitor_idx != self._selected_monitor_idx:
+                        self._selected_monitor_idx = selected_monitor_idx
+                        print('Start streaming')
+                    else:
+                        self._selected_monitor_idx = -1
+                        print('Stop streaming')
+
+            def on_release(self, key):
+                if key in self._pressed_keys:
+                    self._pressed_keys.remove(key)
+
+            @property
+            def selected_monitor_idx(self):
+                return self._selected_monitor_idx
+
         keyboard_state = KeyboardState()
 
-        digit_keys = [keyboard.KeyCode(char=str(i)) for i in range(10)]
-        f_keys = [keyboard.Key.f9, keyboard.Key.f10, keyboard.Key.f11, keyboard.Key.f12]
-        hotkeys = {key:idx for idx, key in sum([list(enumerate(keys)) for keys in [digit_keys, f_keys]], [])} 
+        keyboard_listener = keyboard.Listener(on_press=keyboard_state.on_press, on_release=keyboard_state.on_release)
+        keyboard_listener.start()
+        
 
-        def on_press(key):
-            keyboard_state.pressed_keys.add(key)
-            if keyboard.Key.alt_l in keyboard_state.pressed_keys and key in hotkeys:
-                selected_monitor_idx = hotkeys[key]
-                if selected_monitor_idx != keyboard_state.selected_monitor_idx:
-                    keyboard_state.selected_monitor_idx = selected_monitor_idx
-                    print('Start streaming')
-                else:
-                    keyboard_state.selected_monitor_idx = -1
-                    print('Stop streaming')
+        class MouseState:
+            def __init__(self):
+                self._x = -1
+                self._y = -1
 
-        def on_release(key):
-            if key in keyboard_state.pressed_keys:
-                keyboard_state.pressed_keys.remove(key)
+            def on_move(self, x, y):
+                self._x = x
+                self._y = y
 
-        listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-        listener.start()
+            @property
+            def x(self):
+                return self._x
+
+            @property
+            def y(self):
+                return self._y
+
+        
+        mouse_state = MouseState()
+        mouse_listener = mouse.Listener(on_move=mouse_state.on_move)
+        mouse_listener.start()
+
+
         host = socket.gethostbyname(args.host)
         print('This is the Streaming Server')
         print('Press alt + [F9-F12] to toggle streaming your screens')
 
 
-        class ROISelectorState:
+        class ROISelector:
             def __init__(self):
-                self.p1 = None
-                self.p2 = None
-                self.is_moving = False
+                self._p1 = None
+                self._p2 = None
+                self._is_moving = False
 
-        roi_selector_state = ROISelectorState()
+            def clear_roi(self):
+                self._p1, self._p2 = None, None
+                self._is_moving = False
 
-        def on_mouse(event, x, y, flags, userdata):
-            if event == cv2.EVENT_LBUTTONDOWN:
-                roi_selector_state.p1 = (x,y)
-                roi_selector_state.is_moving = True
+            def on_mouse(self, event, x, y, flags, userdata):
+                if event == cv2.EVENT_LBUTTONDOWN:
+                    self._p1 = (x,y)
+                    self._is_moving = True
+                        
+                elif event == cv2.EVENT_MOUSEMOVE and self._is_moving:
+                    self._p2 = (x,y)
+                elif event == cv2.EVENT_LBUTTONUP:
+                    self._p2 = (x,y)
+                    self._is_moving = False
                     
-            elif event == cv2.EVENT_MOUSEMOVE and roi_selector_state.is_moving:
-                roi_selector_state.p2 = (x,y)
-            elif event == cv2.EVENT_LBUTTONUP:
-                roi_selector_state.p2 = (x,y)
-                roi_selector_state.is_moving = False
-                
-            if event == cv2.EVENT_RBUTTONUP:
-                roi_selector_state.p1, roi_selector_state.p2 = None, None
-                roi_selector_state.is_moving = False
+                if event == cv2.EVENT_RBUTTONUP:
+                    self.clear_roi()
+
+            @property
+            def has_roi(self):
+                return (self.p1 is not None) and (self.p2 is not None)
+
+            @property
+            def x_min(self):
+                return max(min(self.p1[0], roi_selector.p2[0]),0)
+
+            @property
+            def y_min(self):
+                return max(min(roi_selector.p1[1], roi_selector.p2[1]), 0)
+
+            @property
+            def x_max(self):
+                return max(max(roi_selector.p1[0], roi_selector.p2[0]), 0)
+            
+            @property
+            def y_max(self):
+                return max(max(roi_selector.p1[1], roi_selector.p2[1]), 0)
+
+            @property
+            def p1(self):
+                return self._p1
+
+            @property
+            def p2(self):
+                return self._p2
+
+        roi_selector = ROISelector()
 
         with mss() as sct:
             while True:
                 try:
                     if keyboard_state.selected_monitor_idx >= 0 and keyboard_state.selected_monitor_idx < len(sct.monitors):
                         
-                        screen_shot = sct.grab(sct.monitors[keyboard_state.selected_monitor_idx])
-
+                        selected_monitor = sct.monitors[keyboard_state.selected_monitor_idx]
+                        screen_shot = sct.grab(selected_monitor)
+                        
                         img = np.uint8(screen_shot)[...,:3].copy()
                         
-                        if args.scale is not None:
-                            scale = args.scale 
-                        else:
-                            scale = 1080 / img.shape[0]
+                        if not args.hide_cursor:
+                            mouse_x = mouse_state.x - selected_monitor['left']
+                            mouse_y = mouse_state.y - selected_monitor['top']
 
-                        
-                        
-                        if roi_selector_state.p1 is not None and roi_selector_state.p2 is not None:
-                            x_min = np.clip(min(roi_selector_state.p1[0], roi_selector_state.p2[0]), 0, img.shape[1])
-                            y_min = np.clip(min(roi_selector_state.p1[1], roi_selector_state.p2[1]), 0, img.shape[0])
-                            x_max = np.clip(max(roi_selector_state.p1[0], roi_selector_state.p2[0]), 0, img.shape[1])
-                            y_max = np.clip(max(roi_selector_state.p1[1], roi_selector_state.p2[1]), 0, img.shape[0])
+                            if mouse_x >= 0 and mouse_x < img.shape[1] and mouse_y >= 0 and mouse_y <= img.shape[0]:
+                                cv2.arrowedLine(img, (mouse_x+50, mouse_y+50), (mouse_x, mouse_y), (255,255,255), 4, line_type=cv2.LINE_AA, tipLength=0.4)
+
+                        if roi_selector.has_roi:
+                            x_min = roi_selector.x_min
+                            y_min = roi_selector.y_min
+                            x_max = roi_selector.x_max
+                            y_max = roi_selector.y_max
                             
                             if (x_max - x_min) < 10:
                                 x_max += 10 - (x_max - x_min)
@@ -196,11 +260,16 @@ def main():
                                 y_max += 10 - (y_max - y_min)
                             img_roi = img[y_min:y_max+1, x_min:x_max+1].copy()
 
-                            img = cv2.rectangle(img.copy(), roi_selector_state.p1, roi_selector_state.p2, 255, 2)
+                            img = cv2.rectangle(img.copy(), roi_selector.p1, roi_selector.p2, (0,255,0), 5)
 
                         else:
                             img_roi = img
-                        
+
+                        if args.scale is not None:
+                            scale = args.scale 
+                        else:
+                            scale = 1080 / img.shape[0]
+
                         img_resized = cv2.resize(img_roi, (0,0), fx=scale, fy=scale)
 
                         bytes_array = img_to_bytes(img_resized)
@@ -208,9 +277,12 @@ def main():
                         send_bytes(np.int32(len(bytes_array)).tobytes() + bytes_array, ip=host, port=args.port)
                         
                         cv2.namedWindow('select_roi', cv2.WINDOW_NORMAL)
-                        cv2.setMouseCallback('select_roi', on_mouse)
+                        cv2.setMouseCallback('select_roi', roi_selector.on_mouse)
                         cv2.imshow('select_roi', img)
-                        cv2.waitKey(10)
+                        key = cv2.waitKey(10)
+                        if key == 27: # ESC key
+                            roi_selector.clear_roi()
+
                     else:
                         cv2.destroyAllWindows()
                 except KeyboardInterrupt as ex:
